82892216d5c17876967ee37a00ac769a62b2bea7
Laura Flores <lafloresp@gmail.com>
2024-05-17 16:04:28 +0200

    comment yield, change sim.json, add N2Odenit

--- run-consumer.py
+++ run-consumer.py
@@ -91,9 +91,9 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "AtmCO2": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        # "N2Odenit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "N2Odenit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "NH3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "NO3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "NO3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
     }
     output_keys = list(output_grids.keys())
 
