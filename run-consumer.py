263a71c879fe577001e7eddf912bef1331815419
Laura Flores <lafloresp@gmail.com>
2024-05-18 16:11:55 +0200

    update for calculate just yield: 2019

--- run-consumer.py
+++ run-consumer.py
@@ -88,12 +88,12 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
 
     output_grids = {
-        # "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "AtmCO2": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "N2Odenit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "SOC": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "N": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "AtmCO2": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "N2Odenit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "SOC": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "N": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
     }
     output_keys = list(output_grids.keys())
 
