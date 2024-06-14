1f3be0a4737b2a7c89f4207c6156b17646ac472b
Laura Flores <lafloresp@gmail.com>
2024-06-14 19:18:41 +0200

    update

--- run-producer.py
+++ run-producer.py
@@ -92,7 +92,7 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 DATA_GRID_HEIGHT = "germany/dem_100_25832_etrs89-utm32n.asc"
 DATA_GRID_SLOPE = "germany/slope_100_25832_etrs89-utm32n.asc"
 DATA_GRID_SOIL = "germany/buek200_100_25832_etrs89-utm32n.asc"
-
+DATA_GRID_CROPS = "germany/{crop_data}" 
 
 # DATA_GRID_LAND_USE = "germany/landuse_1000_31469_gk5.asc"
 # DATA_GRID_SOIL_OW = "germany/buek200_1000_25832_etrs89-utm32n_OW.asc"
@@ -125,7 +125,7 @@ DEBUG_WRITE_CLIMATE = False
 
 # commandline parameters e.g "server=localhost port=6666 shared_id=2"
 def run_producer(server={"server": None, "port": None}, shared_id=None):
-    DATA_GRID_CROPS = "germany/{crop_data}" 
+    
     context = zmq.Context()
     socket = context.socket(zmq.PUSH)  # pylint: disable=no-member
     # config_and_no_data_socket = context.socket(zmq.PUSH)
