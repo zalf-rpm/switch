12d4236be2ff5251885f796710fb5bc18253c7ea
Laura Flores <lafloresp@gmail.com>
2024-07-05 12:15:37 +0200

    update parameters for Denit

--- run-consumer.py
+++ run-consumer.py
@@ -88,7 +88,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
 
     output_grids = {
-        "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        "Denit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
