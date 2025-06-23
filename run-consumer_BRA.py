3cc195f336a9bb2e110200e3d3f27ad35843b75d
Laura Flores <lafloresp@gmail.com>
2025-06-23 21:40:58 +0200

    new setting to get higher yield for Wrape

--- run-consumer_BRA.py
+++ run-consumer_BRA.py
@@ -83,13 +83,15 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
 
     output_grids = {
-        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "TotBiomN": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "NLeach": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Ra": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "NH3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
+# ,
+#         "TotBiomN": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+#         "NLeach": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+#         "Ra": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+#         "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+#         "NH3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+
     output_keys = list(output_grids.keys())
 
     cmc_to_crop = {}
