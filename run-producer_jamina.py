662090484ad6620d6fcc93d65d92fe53e766f494
Laura Flores <lafloresp@gmail.com>
2025-07-15 21:26:59 +0200

    update name

--- run-producer_jamina.py
+++ run-producer_jamina.py
@@ -62,7 +62,7 @@ PATHS = {
 DATA_SOIL_DB = "germany/buek200.sqlite"
 DATA_GRID_HEIGHT = "germany/NW_dem_100_25832_etrs89-utm32n.asc"
 DATA_GRID_SLOPE = "germany/NW_slope_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SOIL = "germany/NW_soil_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
 
 
 TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
